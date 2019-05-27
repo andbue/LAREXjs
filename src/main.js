0af223e50eb53102113f3749aa02c02bebe28ebb
Andreas Büttner <andbue@gmail.com>
2019-05-27 21:31:48 +0200

    Example html improved

--- src/main.js
+++ src/main.js
@@ -1,5 +1,71 @@
 var test = require('./opencvtest.js');
 document.getElementById('status').innerHTML = 'LAREX.js is ready.';
-document.getElementById("startbutton").addEventListener("click", function (){
+document.getElementById("btnSegment").addEventListener("click", function () {
     test.opencv("svgimage", "svg");
 });
+
+document.getElementById("btnToggleCol").addEventListener("click", function () {
+    let img = document.getElementById("svgimage");
+    if (img.getAttribute("filter") === "url(#filterBW)"){
+        img.removeAttribute("filter");
+    } else {
+        img.setAttribute("filter", "url(#filterBW)");
+    }
+});
+
+
+let svg = document.getElementById("svg");
+let editor = document.getElementById("imgBox");
+
+svg.onmousedown = function(e) {
+    e.preventDefault();
+    let svgBCR = svg.getBoundingClientRect();
+    let edBCR = editor.getBoundingClientRect();
+    let startX = svgBCR.x - edBCR.x - e.clientX;
+    let startY = svgBCR.y - edBCR.y - e.clientY;
+
+    let dragfunc = function(ce) {
+        ce.preventDefault();
+        
+        let minX = Math.min(edBCR.width - svgBCR.width, 0)
+            maxX = Math.max(edBCR.width - svgBCR.width, 0);
+        let left = Math.max(minX, startX + ce.clientX);
+        left = Math.min(maxX, left);
+
+        let minY = Math.min(edBCR.height - svgBCR.height, 0)
+            maxY = Math.max(edBCR.height - svgBCR.height, 0);
+        let top = Math.max(minY, startY + ce.clientY);
+        top = Math.min(maxY, top);
+
+        svg.style.left = left + "px";
+        svg.style.top = top + "px";
+    }
+
+    document.addEventListener("mouseup", function(e){
+        e.preventDefault();
+        editor.removeEventListener("mousemove", dragfunc);});
+    editor.addEventListener("mousemove", dragfunc);
+}
+
+svg.parentElement.onwheel = function(e) {
+    e.preventDefault();
+
+    let scaleFac = 1 + (e.deltaY / 10)
+        svgBCR = svg.getBoundingClientRect()
+        edBCR = editor.getBoundingClientRect()
+        scaleX = (svgBCR.left - e.clientX) / svgBCR.width
+        scaleY = (svgBCR.top - e.clientY) / svgBCR.height
+        startLeft = svgBCR.x - edBCR.x
+        startTop = svgBCR.y - edBCR.y 
+        newH = svgBCR.height * scaleFac
+        newW = svgBCR.width * scaleFac
+        posDiffW = scaleX * (newW - svgBCR.width)
+        posDiffH = scaleY * (newH - svgBCR.height);
+
+    if ((scaleFac > 1 && newH < edBCR.height * 10) ||
+        (scaleFac < 1 && newH > edBCR.height / 2)) {
+        svg.style.height = newH + "px";
+        svg.style.left = (startLeft + posDiffW) + "px";
+        svg.style.top = (startTop + posDiffH) + "px";
+    }
+}
--- src/opencvtest.js
+++ src/opencvtest.js
@@ -4,23 +4,34 @@ var Segmenter = require('./larex/segmentation/Segmenter.js');
 var Parameters = require('./larex/segmentation/parameters/Parameters.js');
 
 module.exports.opencv = function (srcselector, svgselector){
-    let mat = cv.imread(srcselector);
-    let segmenter = new Segmenter(new Parameters());
-    let segmentation = segmenter.segment(mat);
+  let img = document.getElementById(srcselector);
+  let canvas = document.createElement("canvas");
+  let img_w = img.width.baseVal.value;
+  let img_h = img.height.baseVal.value;
+  canvas.width = img_w;
+  canvas.height = img_h;
+  let ctx=canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0, img_w, img_h);
+  
+  let mat = cv.imread(canvas);
+  canvas.remove();
 
-    let svg = document.getElementById(svgselector);
-    while (svg.lastChild.tagName == "polygon") svg.removeChild(svg.lastChild)
-    let svgns = svg.attributes.xmlns.value;
-    for (region of segmentation.regions){
-      let newelement = document.createElementNS(svgns, 'polygon');
-      newelement.setAttribute("id",region.id);
-      newelement.setAttribute("points", region.points.data32S.join(" "));
-      newelement.classList.add(region.type.toString());
-      svg.append(newelement);
-     }
+  let segmenter = new Segmenter(new Parameters());
+  let segmentation = segmenter.segment(mat);
+
+  let svg = document.getElementById(svgselector);
+  while (svg.lastChild.tagName == "polygon") svg.removeChild(svg.lastChild)
+  let svgns = "http://www.w3.org/2000/svg";
+  for (region of segmentation.regions){
+    let newelement = document.createElementNS(svgns, 'polygon');
+    newelement.setAttribute("id",region.id);
+    newelement.setAttribute("points", region.points.data32S.join(" "));
+    newelement.classList.add(region.type.toString());
+    svg.append(newelement);
+    }
 
 
-     segmentation.delete();
-     segmenter.delete();
-     mat.delete();
+    segmentation.delete();
+    segmenter.delete();
+    mat.delete();
   }
